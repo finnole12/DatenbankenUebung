50651b2c8b6b400e3152ccc601588210a9d79611
Finn Ole Hoppe <finn.ole.hoppe@sap.com>
2023-12-11 15:10:32 +0100

    adds F to I

--- python_test_vol/read.py
+++ python_test_vol/read.py
@@ -65,7 +65,259 @@ customerRentalsList.sort(key=takeSecond,reverse=True)
 
 for i in range(10):
     print(f"Kunde: {customerRentalsList[i][0]} Anzahl: {customerRentalsList[i][1]}")
-    
+
+#Aufgabe F
+print("Antwort Aufgabe F:")
+outputF = db.payment.aggregate([
+    {
+        "$group": {
+            "_id": "$customer_id",
+            "sum": {
+                "$sum": {"$toDouble": "$amount" }
+            }
+        }
+    },
+    {
+        "$sort" : {
+            "sum": -1
+        }
+    },
+    {
+        "$limit": 10
+    },
+    {
+        "$lookup": {
+            "from": "customer",
+            "localField": "_id",
+            "foreignField": "customer_id",
+            "pipeline": [
+                {
+                    "$lookup": {
+                        "from": "address",
+                        "localField": "address_id",
+                        "foreignField": "address_id",
+                        "pipeline": [{
+                            "$project": {
+                                "_id": 0,
+                                "address": 1,
+                                "address2": 1,
+                                "city_id": 1,
+                                "district": 1,
+                                "phone": 1,
+                                "postal_code": 1
+                            }
+                        }],
+                        "as": "address"
+                    }
+                },
+                {
+                    "$unwind": "$address"
+                },
+                {
+                    "$project": {
+                        "first_name": 1,
+                        "last_name": 1,
+                        "address": 1,
+                        "customer_id": 1,
+                        "_id": 0
+                    }
+                }
+            ],
+            "as": "customer"
+        }
+    },
+    {
+        "$unwind": "$customer"
+    }, 
+    {
+        "$project": {
+            "first_name": "$customer.first_name",
+            "last_name": "$customer.last_name",
+            "address": "$customer.address",
+            "roundedSum": { "$round": ["$sum", 2] },
+            "_id": 0
+        }
+    }
+])
+import pprint
+pprint.pprint(list(outputF))
+
+# Aufgabe G
+outputG = db.rental.aggregate([
+    {
+        "$lookup": {
+            "from": "inventory",
+            "localField": "inventory_id",
+            "foreignField": "inventory_id",
+            "as": "inventory"
+        }
+    },
+    {
+        "$unwind": "$inventory"
+    },
+    {
+        "$group": {
+            "_id": "$inventory.film_id",
+            "rental_amount": {
+                "$count": { }
+            }
+        }
+    },
+    {
+        "$sort": {
+            "rental_amount": -1
+        }
+    },
+    {
+        "$limit": 10
+    },
+    {
+        "$lookup": {
+            "from": "film",
+            "localField": "_id",
+            "foreignField": "film_id",
+            "pipeline": [
+                {
+                    "$project": {
+                        "_id": 0,
+                        "title": 1
+                    }
+                }
+            ],
+            "as": "film"
+        }
+    },
+    {
+        "$project": {
+            "_id": 0,
+            "title": "$film.title",
+            "rental_amount": 1
+        }
+    }
+])
+pprint.pprint(list(outputG))
+
+#Aufgabe H
+outputH = db.rental.aggregate([
+    {
+        "$lookup": {
+            "from": "inventory",
+            "localField": "inventory_id",
+            "foreignField": "inventory_id",
+            "as": "inventory"
+        }
+    },
+    {
+        "$unwind": "$inventory"
+    },
+    {
+        "$project": {
+            "_id": 0,
+            "film_id": "$inventory.film_id"
+        }
+    },
+    {
+        "$lookup": {
+            "from": "film_category",
+            "localField": "film_id",
+            "foreignField": "film_id",
+            "pipeline": [
+                {
+                    "$project": {
+                        "_id": 0,
+                        "category_id": 1
+                    }
+                }
+            ],
+            "as": "film"
+        }
+    },
+    {
+        "$unwind": "$film"
+    },
+    {
+        "$project": {
+            "category_id": "$film.category_id",
+        }
+    },
+    {
+        "$group": {
+            "_id": "$category_id",
+            "rental_amount": {
+                "$count": { }
+            }
+        }
+    },
+    {
+        "$lookup": {
+            "from": "category",
+            "localField": "_id",
+            "foreignField": "category_id",
+            "pipeline": [
+                {
+                    "$project": {
+                        "_id": 0,
+                        "name": 1
+                    }
+                }
+            ],
+            "as": "category"
+        }
+    },
+    {
+        "$unwind": "$category"
+    },
+    {
+        "$sort": {
+            "rental_amount": -1
+        }
+    },
+    {
+        "$limit": 10
+    },
+    {
+        "$project": {
+            "_id": 0,
+            "category": "$category.name",
+            "rental_amount": 1
+        }
+    }
+])
+pprint.pprint(list(outputH))
+
+#Aufgabe I
+outputI = db.customer.aggregate([
+    {
+        "$lookup": {
+            "from": "address",
+            "localField": "address_id",
+            "foreignField": "address_id",
+            "as": "address"
+        }
+    },
+    {
+        "$unwind": "$address"
+    },
+    {
+        "$project": {
+            "_id": 0,
+            "id": "customer_id",
+            "name": { "$concat": [ "$first_name", " ", "$last_name" ]},
+            "address": "$address.address",
+            "zipcode": "$address.zipcode",
+            "phone": "$address.phone",
+            "city": "$address.city",
+            "country": "$address.country",
+            "notes": {
+                "$cond": {
+                    "if": {  "$eq": ["$active", 1] }, "then": "active", "else": ""
+                }
+            }
+        }
+    }
+])
+import pprint
+pprint.pprint(list(outputI))
 
 print("Reads done. Closing connection.")
 client.close()
